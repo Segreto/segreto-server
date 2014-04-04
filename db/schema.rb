c2d2fbf8c4ea340964bc1bcdd09bf849dd15a3ea
Michael DeWitt <md@hackfoolery.com>
2014-04-04 12:13:32 -0400

    unique usernames

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404150848) do
+ActiveRecord::Schema.define(version: 20140404160958) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20140404150848) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -6,4 +6,21 @@ describe User do
 
   it { should respond_to(:username) }
   it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when username is not present" do
+    before { @user.username = "" }
+    it { should_not be_valid }
+  end
+
+  describe "when username is not unique" do
+    before do 
+      user2 = @user.dup
+      user2.username = user2.username.upcase
+      user2.save
+    end
+
+    it { should_not be_valid }
+  end
 end
