ab33916c07aacc0e47cfa6b979c0bebcb122e310
Josh Cox <joshcox@indiana.edu>
2014-04-08 00:42:45 +0000

    Began writing the user_controller for performing basic actions on users and setting up a session. A couple issues: updating a user in the database.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,26 +1,62 @@
 class UserController < ApplicationController
   before_filter :auth, except: [:create, :signin]
 
+  # show(username)
   def show
+    render json: @user
   end
 
+  # create(username, password&/confirm, email, name)
   def create
+    @new_user = 
+      User.new(
+        username: params[:username],
+        password: params[:password],
+        password_confirmation: params[:password_confirmation],
+        email: params[:email],
+        name: params[:name]
+      )
+
+    @new_user.save
+    render json: @new_user, status: :created 
   end
 
+  # signin(username, password) -> token
   def signin
+    @user = User.find_by username: params[:username]
+    if @user.authenticate
+      token = SecureRandom.urlsafe_base64
+      @user.update(remember_token: token)
+      render json: {remember_token: token}, status: :accepted
+    else
+      render json: {}, status: :forbidden
+    end
   end
 
+  # signout(username)
   def signout
+    @user.update(remember_token: nil)
+    render json: {}, status: :ok
   end
 
+  # update(username, attr*, val*)
   def update
+    
   end
 
+  # destroy(username, password, token)
   def destroy
+    @user.destroy
+    render json: {}, status: :ok
   end
 
   private
 
+  # auth(username, token)
   def auth
+    @user = User.find_by username: params[:username]
+    if @user.remember_token != params[:remember_token]
+      render json: {}, status: :forbidden
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407142248) do
+ActiveRecord::Schema.define(version: 20140408002322) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140407142248) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
